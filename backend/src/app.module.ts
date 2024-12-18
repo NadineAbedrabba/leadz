fdf5559dcb08b20885ca2a93ed864ae14b29b56a
Your Name <you@example.com>
2024-12-18 09:31:09 +0100

    liaison back avec base

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,36 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, } from '@nestjs/config';
+
+
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+     ConfigModule.forRoot({ isGlobal: true }),
+     TypeOrmModule.forRoot(
+ {
+ type: 'mysql',
+ host: 'localhost',
+ port: 3306,
+ username: 'root',
+ password: '',
+ database: 'leadz',
+ entities: [],
+ synchronize: true,
+ }
+ )
+  ],
   controllers: [AppController],
   providers: [AppService],
-})
-export class AppModule {}
+}
+)
+
+export class AppModule {
+  
+}
