65dfa4f4c184e0e45c33a14bb11c33288336c050
ilef kristou <ilefkristou992@gmail.com>
2024-12-20 11:51:54 +0100

    dev backend partie gestion contacts/projets

---
+++ backend/src/contact/contact.service.ts
@@ -0,0 +1,41 @@
+// src/services/contact.service.ts
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Contact } from './contact.entity/contact.entity';
+
+@Injectable()
+export class ContactService {
+  constructor(
+    @InjectRepository(Contact)
+    private readonly contactRepo: Repository<Contact>,
+  ) {}
+
+  createContact(data: Partial<Contact>) {
+    const contact = this.contactRepo.create(data);
+    return this.contactRepo.save(contact);
+  }
+
+  getAllContacts() {
+    return this.contactRepo.find();
+  }
+
+  async getAllContactsOnly(): Promise<Contact[]> {
+    return this.contactRepo.find({
+      where: { typeContact: 'contact' }, // Filtrer uniquement les contacts de type 'Contact'
+    });
+  }
+
+  getContactById(id: number) {
+    return this.contactRepo.findOne({ where: { id } });
+  }
+
+  async updateContact(id: number, data: Partial<Contact>) {
+    await this.contactRepo.update(id, data);
+    return this.contactRepo.findOne({ where: { id } });
+  }
+
+  deleteContact(id: number) {
+    return this.contactRepo.delete(id);
+  }
+}
